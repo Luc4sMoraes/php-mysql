265be0af280c62bb06c47d0d2cc59d236ef6c3e7
root <root@Debian.nigtnt01.ap.so-net.ne.jp>
2017-05-19 18:00:47 -0300

    Sobre o sistema, pagina inicial

--- index.php
+++ index.php
@@ -1,45 +1,54 @@
-<?php
-
-function __autoload($class_name){
-	require_once "classes/" . $class_name . ".php";
-}
-
-$usuario = new Usuarios();
-
-if(isset($_POST["cadastrar"])){
-	$nome = $_POST["nome"];
-	$idade = $_POST["idade"];
-	$sexo = $_POST["sexo"];
-
-	$usuario->setNome($nome);
-	$usuario->setIdade($idade);
-	$usuario->setSexo($sexo);
-	
-	# insert 
-	if($usuario->insert()){
-		echo "inserido com sucesso!";
-	}
-	
-
-}
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="pt-br"></html>
 <head>
-	<title></title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css"/>
+    <script type="text/javascript" src="_script/Validar.js"></script>
+    <!-- Bootstrap -->
+   <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+	<title>Pagina inicial</title>
+	<style type="text/css">
+		h1 {
+			text-align: center;
+			opacity:0.7;
+		}
+		article{
+			width: 80%;
+			margin: 0 auto;
+			padding: 5px;
+		}
+		article p {
+			font-size: 14px;
+			font-family: arial;
+			color:#363636;
+
+		}
+
+	</style>
 </head>
 <body>
+<div id="menu">
+<div class="btn-group" role="group" aria-label="...">
+  <button type="button" class="btn btn-default" "><a href="select.php">Select</a></button>
+  <button type="button" class="btn btn-default"><a href="update.php">Update</a></button>
+  <button type="button" class="btn btn-default"><a href="delete.php">Delete</a></button>
+</div>
+</div>
 	
-<form action="" method="post">
-Nome <input type="text" name="nome">
-Idade <input type="number" name="idade">
-Sexo <input type="text" name="sexo">
-<input type="submit" name="cadastrar" value="Cadastrar">
+<h1>Sistema de cadastro de usuarios simples</h1>
+<article>
+<p>
+	Realizado com base nas aulas do professor Guanabara. 
+	O sistema consiste em cadastrar, alterar e remover usuarios. É possivel mesclar select/update/delete porem para fins de aprendizagem não o fiz.
+	</br>
+	</br>
+	#php/mysql/javascript/css/bootstrap
+	
 
-
+</p>
+</article>
 </form>
 
 </body>
